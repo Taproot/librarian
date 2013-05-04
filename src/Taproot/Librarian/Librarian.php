68cad24b1a9aef25c6848c54c440953696dd3808
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-05-04 19:30:37 +0000

    Added dummy implementations of all the interface methods

--- src/Taproot/Librarian/Librarian.php
+++ src/Taproot/Librarian/Librarian.php
@@ -3,5 +3,36 @@
 namespace Taproot\Librarian;
 
 class Librarian implements LibrarianInterface {
+	public function __construct($namespace, array $config = []) {
 	
+	}
+	
+	public function addIndexes(array $indexes) {
+	
+	}
+	
+	public function buildEnvironment() {
+	
+	}
+	
+	
+	public function buildIndexes() {
+	
+	}
+	
+	public function get($id) {
+	
+	}
+	
+	public function put(array $item) {
+	
+	}
+	
+	public function delete($id) {
+	
+	}
+	
+	public function query($limit = 20, $orderBy = []) {
+	
+	}
 }
