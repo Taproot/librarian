125f5e6692ade0886a782f5dbdbde178e713b13c
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-05-04 20:03:17 +0000

    Created first listeners, added rudimentary tests

--- tests/Taproot/Librarian/LibrarianTest.php
+++ tests/Taproot/Librarian/LibrarianTest.php
@@ -2,10 +2,30 @@
 
 namespace Taproot\Librarian\Test;
 
-use Taproot\Librarian\Librarian;
+use Taproot\Librarian as L;
 
 class LibrarianTest extends \PHPUnit_Framework_TestCase {
-	public function testLibrarianClassExists() {
-		$l = new Librarian('test');
+	private $l;
+	
+	public function setUp() {
+		parent::setUp();
+		$this->l = new L\Librarian('test', []);
+	}
+	
+	public function testSavedDataCanBeRetrieved() {
+		$data = [
+			'id' => '1',
+			'name' => 'The Test Data'
+		];
+		
+		$this->l->put($data);
+		
+		try {
+			$result = $this->l->get('1');
+		} catch (L\CrudException $e) {
+			$this->fail('Got a CrudException with message "' . $e->getMessage() . '"');
+		}
+		
+		$this->assertEquals($data, $result);
 	}
 }
